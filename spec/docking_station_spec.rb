ee1bfe4b6a1bd8e59d2ad7bbb071c64032db24b8
Basile Koko <basile_koko@hotmail.com>
2017-01-04 21:00:13 +0000

    exercise 10 add test to docking_station_spec.rb  and working method to Bike class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,8 @@
 require './lib/docking_station'
 describe DockingStation do
 it {is_expected.to respond_to :release_bike}
+it 'should respond to working method' do
+  new_instance = DockingStation.new.release_bike
+  expect(new_instance).to respond_to :working?
+end
 end
